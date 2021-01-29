963cfb63577a163010e844d7c1ac89f3ba260241
christopher <angel.rmarin@outlook.com>
2021-01-28 18:49:33 -0600

    move code into component table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component, QueryList, ViewChildren } from '@angular/core';
+import { Component } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 
 import { RestaurantsService } from './restaurants.service';
-import { NgbdSortableHeader, SortEvent } from './sortable.directive';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,25 +12,11 @@ export class AppComponent {
   restaurants$: Observable<any>;
   total$: Observable<number>;
 
-  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
-
   constructor(public restaurantsService: RestaurantsService) {
     this.restaurants$ = restaurantsService.restaurants$;
     this.total$ = restaurantsService.total$;
     restaurantsService.getRestaurants().subscribe((res) => {
       console.log(res);
     });
-  }
-
-  onSort({ column, direction }: SortEvent) {
-    // resetting other headers
-    this.headers.forEach((header) => {
-      if (header.sortable !== column) {
-        header.direction = '';
-      }
-    });
-
-    this.restaurantsService.sortColumn = column;
-    this.restaurantsService.sortDirection = direction;
   }
 }
--- src/app/restaurants-table/restaurants-table.component.ts
+++ src/app/restaurants-table/restaurants-table.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
+import { NgbdSortableHeader, SortEvent } from '../sortable.directive';
+import { RestaurantsService } from '../restaurants.service';
 
 @Component({
   selector: 'app-restaurants-table',
   templateUrl: './restaurants-table.component.html',
-  styleUrls: ['./restaurants-table.component.css']
+  styleUrls: ['./restaurants-table.component.css'],
 })
 export class RestaurantsTableComponent implements OnInit {
+  @Input() restaurantsData;
+  @Input() total;
 
-  constructor() { }
+  @ViewChildren(NgbdSortableHeader) headers: QueryList<NgbdSortableHeader>;
 
-  ngOnInit(): void {
+  constructor(public restaurantsService: RestaurantsService) {}
+
+  ngOnInit(): void {}
+
+  onSort({ column, direction }: SortEvent) {
+    // resetting other headers
+    this.headers.forEach((header) => {
+      if (header.sortable !== column) {
+        header.direction = '';
+      }
+    });
+
+    this.restaurantsService.sortColumn = column;
+    this.restaurantsService.sortDirection = direction;
   }
-
 }
